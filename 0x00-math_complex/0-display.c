6332f7b98f9e277e50a80e7ad499626b8078d560
MontassarBar <montassarbarbouchi7@gmail.com>
2021-12-14 13:42:31 +0100

    displays the complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,6 +8,12 @@ void display_complex_number(complex c)
 {
 if (c.im == 0)
 printf("%d\n", c.re);
+else if (c.im < 0)
+printf("%d - %di", c.re, c.im);
+else if (c.im == 1)
+printf("%d + i", c.re);
+else if (c.im == -1)
+printf("%d - i", c.re);
 else
 printf("%d + %di\n", c.re, c.im);
 }
